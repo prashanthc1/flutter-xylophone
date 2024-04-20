7f33852611f6bb0a2ff035fe38500a20ad2b9230
Prashanth <prashanth.cheepelli@yahoo.com>
2024-04-20 10:51:38 +0400

    Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -36,19 +37,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            buildKey(1, Colors.red, "Red"),
-            buildKey(2, Colors.orange, "Orange"),
-            buildKey(3, Colors.yellow, "Yellow"),
-            buildKey(4, Colors.green, "Green"),
-            buildKey(5, Colors.teal, "Teal"),
-            buildKey(6, Colors.blue, "Blue"),
-            buildKey(7, Colors.purple, "Purple"),
-          ],
+      appBar: AppBar(
+        title: Text(
+          "Xylophone",
+          style: GoogleFonts.roboto(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+            letterSpacing: 3.5,
+          ),
         ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+                Color(0xFF2196F3),
+                Color.fromARGB(183, 60, 13, 190),
+                Color.fromARGB(255, 38, 35, 231),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildKey(1, Colors.red, "Red"),
+          buildKey(2, Colors.orange, "Orange"),
+          buildKey(3, Colors.yellow, "Yellow"),
+          buildKey(4, Colors.green, "Green"),
+          buildKey(5, Colors.teal, "Teal"),
+          buildKey(6, Colors.blue, "Blue"),
+          buildKey(7, Colors.purple, "Purple"),
+        ],
       ),
     );
   }

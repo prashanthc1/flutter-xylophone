47c9d6261e3eb3f65915af7c6ddfb3cb519409bd
Prashanth <prashanth.cheepelli@yahoo.com>
2024-09-03 22:05:34 +0400

    ok

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
-// 📦 Package imports:
 import 'package:flutter_test/flutter_test.dart';
 
-// 🌎 Project imports:
 import 'package:xylophone/main.dart';
 
 void main() {
